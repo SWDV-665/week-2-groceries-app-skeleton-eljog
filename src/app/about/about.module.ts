250659186e8c84dbc9e251ed19a3438631b0a681
Eljo George <egeorge1@live.maryville.edu>
2021-03-20 20:34:23 -0700

    Meaningful names for tab components

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -3,10 +3,10 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Tab2Page } from './tab2.page';
+import { AboutPage } from './about.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
-import { Tab2PageRoutingModule } from './tab2-routing.module';
+import { AboutPageRoutingModule } from './about-routing.module';
 
 @NgModule({
   imports: [
@@ -14,8 +14,8 @@ import { Tab2PageRoutingModule } from './tab2-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab2PageRoutingModule
+    AboutPageRoutingModule
   ],
-  declarations: [Tab2Page]
+  declarations: [AboutPage]
 })
-export class Tab2PageModule {}
+export class AboutPageModule {}
